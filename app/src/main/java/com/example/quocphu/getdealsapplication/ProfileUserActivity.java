3e6ff11ccdf0f306fade484eadb65308746f147c
chaunhattruongit <chaunhattruongit@gmail.com>
2018-11-14 08:28:30 +0700

    layout oneitem listdeal and profile user
    create layout

---
+++ app/src/main/java/com/example/quocphu/getdealsapplication/ProfileUserActivity.java
@@ -0,0 +1,13 @@
+package com.example.quocphu.getdealsapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ProfileUserActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_user);
+    }
+}
