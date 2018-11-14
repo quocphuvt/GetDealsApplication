1533d27e31421b3e2e10d0c8e54364b520cbc7e9
chaunhattruongit <chaunhattruongit@gmail.com>
2018-11-14 11:32:44 +0700

    edit
    do it

---
+++ app/src/main/java/com/example/quocphu/getdealsapplication/PostDealActivity.java
@@ -0,0 +1,13 @@
+package com.example.quocphu.getdealsapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PostDealActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_deal);
+    }
+}
